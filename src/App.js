12d5daee3eb3ede4f44781034dd1d304804cb6d1
Eric Chee <ericchee@hotmail.co.uk>
2023-10-27 20:36:08 +0100

    added light and dark theme and font style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import './styles/Theme.css';
+import { createContext, useState, useEffect } from "react";
+import iconMoon from "./images/icon-moon.svg";
+import iconSun from "./images/icon-sun.svg";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.body.style.backgroundColor = theme === "dark" ? "hsl(235, 21%, 11%)" : "hsl(0, 0%, 98%)";
+  }, [theme]);
+
+  
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-      <div className="container">
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="app" id={theme}>
+        <div className="container" >
+          <div className="header">
+              <div className='title'>TODO</div>
+              <div className='theme-toggle' onClick={toggleTheme} ><img className="illustration-signup-desktop" src={theme === "light" ? iconMoon : iconSun} alt="" /></div>
+          </div>
+        </div>
       </div>
+    </ThemeContext.Provider>
   );
 }
 
