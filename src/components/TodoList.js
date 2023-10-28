8f9b19ba8a7871017cf83cfd65aefa4280bec1a6
Eric Chee <ericchee@hotmail.co.uk>
2023-10-28 22:20:55 +0100

    implement JSON-Server and get request using useState() and useEffect()

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,8 +3,27 @@ import '../styles/Theme.css';
 import "../styles/Todo.css"
 import iconCheck from "../images/icon-check.svg"
 import iconCross from "../images/icon-cross.svg"
+import { useState, useEffect } from 'react';
 
 const TodoList = () => {
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=> {
+    const getTasks = async  () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer)
+    }
+    getTasks();
+  },[])
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    console.log(data)
+    return data;
+  };
+
   return (
     <div className='todo-container'>
         <form action="">
